a08a627b58e01b29e915861226e8d5e1c96c95f1
Bianca <bianca.ionescu@roinspace.com>
2022-12-27 14:52:44 +0200

    Removed the build directory

--- build/_deps/catch-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Bianca/SpotifyStream/build/_deps/catch-src
-# Build directory: /mnt/c/Users/Bianca/SpotifyStream/build/_deps/catch-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Bianca/SpotifyStream/tests
-# Build directory: /mnt/c/Users/Bianca/SpotifyStream/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(testlibtest "/mnt/c/Users/Bianca/SpotifyStream/build/tests/testlib")
-set_tests_properties(testlibtest PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Bianca/SpotifyStream/tests/CMakeLists.txt;10;add_test;/mnt/c/Users/Bianca/SpotifyStream/tests/CMakeLists.txt;0;")
-subdirs("../_deps/catch-build")
