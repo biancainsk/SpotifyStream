89feab5a3990e3ddfa5eb0f49c65671092889ba0
Bianca <bianca.ionescu@roinspace.com>
2023-02-22 22:45:27 +0200

    Added getSongData method

--- app/main.cpp
+++ app/main.cpp
@@ -3,8 +3,8 @@
 
 int main(){
 
-    std::string clientID{"..."}; // Enter your clientID for the Spotify Developer account
-    std::string clientSecret{"..."}; // Enter your clientSecret for the Spotify Developer account
+    std::string clientID{""}; // Enter your clientID for the Spotify Developer account
+    std::string clientSecret{""}; // Enter your clientSecret for the Spotify Developer account
     std::string artist{"Mayhem"};
     std::string track{"Freezing Moon"};
     std::string market{"RO"};
@@ -14,6 +14,7 @@ int main(){
 
     AudioStream stream1;
     stream1.saveSong("https://p.scdn.co/mp3-preview/e191464008685582d91fb4a3a33d064021beb96b?cid=72b709a3e58646e69cad9ace78bb6269");
+    stream1.getSongData();
 
     return 0;
 }
