11570d89a85d827ff1558aa2f51f9c9ab06e0936
Bianca <bianca.ionescu@roinspace.com>
2023-02-22 18:04:40 +0200

    Local commit

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,10 @@
-#include <getURL.h>
-#include <streamURL.h>
+#include <SpotifyAPI.h>
+#include <AudioStream.h>
 
 int main(){
 
-    std::string clientID{""}; // Enter your clientID for the Spotify Developer account
-    std::string clientSecret{""}; // Enter your clientSecret for the Spotify Developer account
+    std::string clientID{"..."}; // Enter your clientID for the Spotify Developer account
+    std::string clientSecret{"..."}; // Enter your clientSecret for the Spotify Developer account
     std::string artist{"Mayhem"};
     std::string track{"Freezing Moon"};
     std::string market{"RO"};
@@ -12,7 +12,7 @@ int main(){
     SpotifyAPI song1(clientID, clientSecret, artist, track, market);
     song1.getURL();
 
-    audioStream stream1;
+    AudioStream stream1;
     stream1.saveSong("https://p.scdn.co/mp3-preview/e191464008685582d91fb4a3a33d064021beb96b?cid=72b709a3e58646e69cad9ace78bb6269");
 
     return 0;
